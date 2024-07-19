70ed8de41f9deb71a4cd77259812ed14770a25d4
unknown <aqshalzuhdi2837@gmail.com>
2024-07-19 15:07:38 +0700

    add query to initialized after (in column)

--- example/src/example/add_columns example.js
+++ example/src/example/add_columns example.js
@@ -2,27 +2,31 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.sequelize.transaction(t => {
-			return Promise.all([
-				queryInterface.addColumn(
-					'users',
-					'address',
-					{
-						type: Sequelize.DataTypes.STRING,
-					},
-					{ transaction: t },
-				),
-				queryInterface.addColumn(
-					'users',
-					'favoriteColor',
-					{
-						type: Sequelize.DataTypes.STRING,
-					},
-					{ transaction: t },
-				),
-			]);
-		});
+	async up(queryInterface, Sequelize) {
+		await queryInterface.addColumn('Users', 'refresh_token', {
+			type: Sequelize.TEXT,
+			after: 'id'
+		})
+		// return queryInterface.sequelize.transaction(t => {
+		// 	return Promise.all([
+		// 		queryInterface.addColumn(
+		// 			'users',
+		// 			'address',
+		// 			{
+		// 				type: Sequelize.DataTypes.STRING,
+		// 			},
+		// 			{ transaction: t },
+		// 		),
+		// 		queryInterface.addColumn(
+		// 			'users',
+		// 			'favoriteColor',
+		// 			{
+		// 				type: Sequelize.DataTypes.STRING,
+		// 			},
+		// 			{ transaction: t },
+		// 		),
+		// 	]);
+		// });
 	},
 	down: (queryInterface, Sequelize) => {
 		return queryInterface.sequelize.transaction(t => {
